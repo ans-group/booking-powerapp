14fe73f4ee0d5b6c7f7532d5ef5a23cdd874376c
Nathan Gaskill <nathang@ANSGRP-002830.local>
2020-06-25 08:29:01 +0100

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# booking-powerapp
-Booking Application built with Microsoft PowerApps
+![ANS](./images/ans_logo_small.png)
+
+# PowerApp Booking application
+
+For more information on the PowerApps Booking Application visit <https://www.ans.co.uk>
+
+The application is deployed as a PowerApp solution file and requires an Azure SQL Database to store the data. 
+
+## Pre-Requisites
+
+The solution requires you have the following resources and permissions, during the installation.
+
+• A Microsoft Azure Subscription and permissions to the subscription as Contributor or Owner.
+• A Microsoft PowerApps Environment with available licenses.
+• PowerApps User/App licensing.
+
+## Azure SQL Database
+
+To create an Azure SQL Database,
+
+1. logon to the Azure Portal with an account that has **Contributor** or **Owner** permissions: https://portal.azure.com
+
+2. Open the hamburger menu icon on the top left of the portal and click on **+ Create a resource**
+
+3. In the search box type **SQL Database**, and click on it in the list of options.
+
+4. Click on the **Create button**.
+
+5. On the Create SQL Databse form complete the required information
+
+    * Subscription: (Choose an available subscription to use)
+
+    * Resource group: (Create or choose an existing resource group)
+
+    * Databse Name: (Use a name thats meaningful name, we use pp-booking-app-sql-db)
+
+    * Server: (Either use an existing or create a new server)
+
+    * Want to use SQL elastic pool? (No)
+
+    * Compute + Storage: (You can start with a Basic 2GB Storage for testing purposes))
+
+6. Click on **Review + create**
+
+7. Click on **Create**, and wait a few minuets for the resources to be created.
+
+8. Open the hamburger menu icon on the top left of the portal and click on **SQL Databases**.
+
+9. From the list of Databases click on the name of the one you have just created.
+
+10. From the menu on the left click on **Query editor (preview)** 
+
+11. Enter the logon details for the SQL server, and click **OK** button.
+
+12. Copy the contents of the powerapp-ans-booking-app.sql file provided, into the query window and click on **> Run**.
+
+### Create a PowerApps Environment
+
+The application is deployed into a PowerApps Environment, you can use an existing environment or to create a new PowerApps Environment follow the steps below.
+
+1. Login to https://admin.powerplatform.microsoft.com/environments
+
+2. Click on the **+ New** button and create new Environment. Use a name that makes it easy to identify the environments purpose and include Prod or Dev to identify if it is production or development.
+
+   - **Name:** **_Booking App - Prod_**
+   - **Type:** Production
+   - **Region:** United Kingdom
+   - **Create a database:** Yes
+
+   - **Language:** English
+   - **Currency:** GBP (£)
+
+### Deploy PowerApps Solution
+
+The PowerApp solution
+
+1. Download the Booking PowerApps solution zip file here: https://github.com/ans-group/booking-powerapp/raw/master/ansbookingapp_1_0_0_2.zip
+
+2. Open https://make.powerapps.com
+
+3. From the top menu select the environment that the solution will be deployed into, this is the one we created earlier.
+
+4. On the left-hand menu click on **Solutions**, then from the top menu click on **Import**, a new window will open.
+
+5. Click on **Choose File** and select the file you downloaded in step 1, then click **Next** button.
+
+6. Click **Next** button again, then click **Import** button and wait for solution customisations to import.
+
+7. If you receive a warning that relates to the below two items, it can safely be ignored.
+
+   - Inbound Bot Webhook
+   - Send email on new request
+
+8. Click on the **Close** button, to complete, and **ans-booking-app** should now appear in the list of solutions.
+
+### Update Power Automate Flow connections
+
+To update the Power Automate Flow connections.
+
+1. Open https://make.powerapps.com
+
+2. From the top menu select the environment that the solution will be deployed into, this is the one we created earlier.
+
+3. Click on the **ans-booking-app** solution, then find **PowerApp -> Get Available Spots** in the list of solution components and click on it. This will open a Power Automate window.
+
+4. On the top menu click on **Edit**, each component in the Flow will be displaying a warning triangle click on the top component, to open it.
+
+5. Click on **+Add new connection** link, it should automatically sign you in and create a new connection.
+
+6. Click on the second component to open it, then click on **Common Data Service link** to re-use the same connection created on the first component.
+
+7. Click on the third component to open it, then click on **Common Data Service link** to re-use the same connection created on the first component.
---
+++ powerapp-ans-booking-app.sql
@@ -0,0 +1,335 @@
+-- SQL Script to create required Tables, Views and Stored Procedures for PowerApp ANS Booking App.
+
+-- Create Buildings Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_buildings](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[building_name] [varchar](50) NOT NULL,
+	[address_line1] [varchar](50) NULL,
+	[address_line2] [varchar](50) NULL,
+	[address_line3] [varchar](50) NULL,
+	[address_city] [varchar](20) NULL,
+	[address_country] [varchar](20) NULL,
+	[address_postcode] [varchar](10) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_buildings] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+-- Create Notes Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_notes](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[note_name] [varchar](50) NOT NULL,
+	[text] [varchar](2048) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_notes] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+-- Create Reservations Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_reservations](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[spot_id] [int] NOT NULL,
+	[reservation_date] [date] NOT NULL,
+	[space_option_id] [int] NOT NULL,
+	[outlook_id] [varchar](200) NULL,
+	[user_id] [int] NOT NULL,
+	[reservation_state] [bit] NULL,
+	[reservation_start_time] [time](0) NOT NULL,
+	[reservation_end_time] [time](0) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_reservations] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_reservations] ADD  CONSTRAINT [reservation_state_cs]  DEFAULT ((1)) FOR [reservation_state]
+GO
+
+-- Create Space Options Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_space_options](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[space_id] [int] NOT NULL,
+	[option_name] [varchar](50) NOT NULL,
+	[option_start_time] [time](0) NULL,
+	[option_end_time] [time](0) NULL,
+	[options_day_sunday] [bit] NULL,
+	[options_day_monday] [bit] NULL,
+	[options_day_tuesday] [bit] NULL,
+	[options_day_wednesday] [bit] NULL,
+	[options_day_thursday] [bit] NULL,
+	[options_day_friday] [bit] NULL,
+	[options_day_saturday] [bit] NULL,
+	[space_option_enabled] [bit] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_space_options] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+-- Create Spaces Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_spaces](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[building_id] [int] NOT NULL,
+	[space_name] [varchar](50) NOT NULL,
+	[space_capacity] [int] NULL,
+	[floor_number] [varchar](50) NULL,
+	[space_enabled] [bit] NULL,
+	[space_notes] [varchar](2048) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_spaces] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+-- Create Spots Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_spots](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[space_id] [int] NOT NULL,
+	[spot_name] [varchar](50) NOT NULL,
+	[spot_sanitised] [bit] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_spots] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_spots] ADD  CONSTRAINT [spot_sanitised_cs]  DEFAULT ((0)) FOR [spot_sanitised]
+GO
+
+-- Create Users Table
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ans_booking_users](
+	[id] [int] IDENTITY(100001,1) NOT NULL,
+	[full_name] [varchar](50) NOT NULL,
+	[email] [varchar](100) NOT NULL,
+	[accepted_terms] [bit] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ans_booking_users] ADD PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+ALTER TABLE [dbo].[ans_booking_users] ADD  CONSTRAINT [uc_email] UNIQUE NONCLUSTERED 
+(
+	[email] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+
+-- Create Reservations View
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[ans_bookings_reservations_view]
+AS
+SELECT res.[id]
+      ,spot.[spot_name]
+      ,spot.[id] AS spot_id
+      ,spot.[spot_sanitised]
+      ,space.[space_name]
+      ,space.[id] AS space_id
+      ,building.[building_name]
+      ,building.[id] AS building_id
+      ,res.[reservation_date]
+      ,res.[reservation_start_time] AS [option_start_time]
+      ,res.[reservation_end_time] AS [option_end_time]
+      ,res.[reservation_state]
+      ,res.[outlook_id]
+      ,users.[full_name]
+      ,users.[email]
+      ,users.[id] AS user_id
+
+FROM ((((([dbo].[ans_booking_reservations] res
+INNER JOIN [dbo].[ans_booking_spots] spot ON res.spot_id = spot.id)
+INNER JOIN [dbo].[ans_booking_space_options] opt ON res.space_option_id = opt.id)
+INNER JOIN [dbo].[ans_booking_spaces] space ON spot.space_id = space.id)
+INNER JOIN [dbo].[ans_booking_buildings] building ON space.building_id = building.id)
+INNER JOIN [dbo].[ans_booking_users] users ON res.user_id = users.id)
+GO
+
+-- Create Stored Procedures Update Space Capcity
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[ans_booking_spaces_update_capacity_sp] 
+    @space_id INT
+
+AS
+BEGIN
+    DECLARE @capacity INT
+    SET @capacity = (SELECT COUNT(*) FROM [dbo].[ans_booking_spots] WHERE space_id = @space_id)
+
+    UPDATE [dbo].[ans_booking_spaces] 
+    SET space_capacity = @capacity
+    WHERE id = @space_id
+END;
+GO
+
+-- Create Stored Procedures Update Spot Availability
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[ans_booking_spots_availability_sp] 
+    @space_id INT,
+    @start TIME(0),
+    @end TIME(0),
+    @date DATE
+
+AS
+BEGIN
+    SELECT
+        spot.id,
+        spot.spot_name,
+        spot.spot_sanitised
+
+    FROM [ans_booking_spots] spot
+        LEFT JOIN [ans_booking_spaces] spaces ON spot.space_id = spaces.id  
+
+    WHERE spot.space_id = @space_id
+
+    EXCEPT
+    SELECT
+        res.spot_id,
+        spot.spot_name,
+        spot.spot_sanitised
+    FROM 
+        [ans_booking_reservations] res
+        LEFT JOIN [ans_booking_space_options] opt ON res.space_option_id = opt.id
+        LEFT JOIN [ans_booking_spots] spot ON res.spot_id = spot.id
+
+    WHERE res.reservation_date = @date 
+        AND spot.space_id = @space_id
+        AND res.reservation_state = 1
+        AND (@end >= opt.option_start_time AND @start <= opt.option_end_time);
+END;
+GO
+
+-- Create Stored Procedures Check Spot Status
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[ans_booking_spots_check_status_sp] 
+    @current_time TIME(0),
+    @current_date DATE
+
+AS
+BEGIN
+    SELECT *
+    FROM [dbo].[ans_bookings_reservations_view] res
+    WHERE res.reservation_date = @current_date AND @current_time >= DATEADD(MINUTE, -15, res.option_start_time) AND @current_time <= DATEADD(MINUTE, -10, res.option_start_time)
+END;
+GO
+
+-- Create Stored Procedures Create Spot Reservation
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ans_booking_spots_reservation_sp] 
+    @spot_id INT,
+    @option_id INT,
+    @date DATE,
+    @user_id INT,
+    @outlook_id VARCHAR(200)
+
+AS
+BEGIN
+    DECLARE @available_spot_id INT;
+
+    SET @available_spot_id = (
+        SELECT
+            spot_id
+        FROM 
+            [dbo].[ans_booking_reservations]
+        WHERE
+            reservation_date = @date 
+            AND spot_id = @spot_id 
+            AND space_option_id = @option_id
+    )
+
+    SELECT @available_spot_id;
+    
+    IF @available_spot_id IS NULL 
+
+    DECLARE @reservation_start_time TIME(0)
+    DECLARE @reservation_end_time TIME(0)
+
+    SET @reservation_start_time = (SELECT option_start_time FROM [dbo].[ans_booking_space_options] WHERE id = @option_id)
+    SET @reservation_end_time = (SELECT option_end_time FROM [dbo].[ans_booking_space_options] WHERE id = @option_id)
+
+    BEGIN
+        INSERT INTO [dbo].[ans_booking_reservations] (spot_id, user_id, reservation_date, reservation_start_time, reservation_end_time, space_option_id, outlook_id)
+        VALUES(@spot_id, @user_id, @date, @reservation_start_time, @reservation_end_time, @option_id, @outlook_id)
+    END;
+
+END;
+GO
+
+-- Create Stored Procedures Update Date Spot Sanitised
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[ans_booking_spots_update_not_sanitised_sp] 
+    @current_time TIME(0),
+    @current_date DATE
+
+AS
+BEGIN
+    UPDATE [dbo].[ans_booking_spots]
+    SET [dbo].[ans_booking_spots].[spot_sanitised] = 0
+
+    FROM (([dbo].[ans_booking_reservations] res
+    INNER JOIN [dbo].[ans_booking_space_options] opt ON res.space_option_id = opt.id)
+    INNER JOIN [dbo].[ans_booking_spots] spot ON res.spot_id = spot.id)
+    WHERE  @current_time >= DATEADD(MINUTE, -15, opt.option_end_time) AND @current_time <= DATEADD(MINUTE, -10, opt.option_end_time) AND res.reservation_date = @current_date
+END;
+GO
